1ceae81a3eb7bcc1958f406d83e2a4442915ef26
Alexey <cache.rocks@gmail.com>
2019-10-16 00:05:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt install -y nodejs libavahi-compat-libdnssd-dev
 [ ! -d ~/.homebridge ] && mkdir ~/.homebridge && cp config.json ~/.homebridge
 
 sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
